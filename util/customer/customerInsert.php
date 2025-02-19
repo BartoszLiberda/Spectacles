2a55c176f5f824dbd8b569b3a01200993eafd4f3
BartoszLiberda <C00295791@setu.ie>
2025-02-19 21:23:40 +0000

    sql query date fix

--- util/customer/customerInsert.php
+++ util/customer/customerInsert.php
@@ -2,11 +2,10 @@
     include('../db.inc.php');
     date_default_timezone_set("UTC");
     
-    $dob = date_create($_POST['dob']);
     $id = "TEST";
     
     $sql = "Insert into Customer (CustomerID,FirstName,Surname,Eircode,Address,DateOfBirth,PhoneNumber,Del_Tag)
-    VALUES ('$id','$_POST[fname]','$_POST[sname]','$_POST[eircode]','$_POST[address]','$dob','$_POST[phone]','0')";
+    VALUES ('$id','$_POST[fname]','$_POST[sname]','$_POST[eircode]','$_POST[address]','$_POST[$dob]','$_POST[phone]','0')";
 
     if(!mysqli_query($con,$sql))
     {
