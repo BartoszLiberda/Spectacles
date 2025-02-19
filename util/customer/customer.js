4912e6dca25ddd1a5f98e8db0db0b900a3f9269b
BartoszLiberda <C00295791@setu.ie>
2025-02-19 21:11:21 +0000

    typo fix

--- util/customer/customer.js
+++ util/customer/customer.js
@@ -1,6 +1,6 @@
 function addCustomer(){
     if(confirm("Are You Sure You Want To Proceed ?")){
-        document.getElementById("addCustomer").onsubmit();
+        document.getElementById("addCustomer").submit();
     }else{
         return;
     }
