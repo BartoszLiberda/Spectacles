683d606c2ccb7357efc94d7bbb46543f370c2078
BartoszLiberda <C00295791@setu.ie>
2025-02-23 18:14:06 +0000

    test

--- pages/maintenance/customer.html.php
+++ pages/maintenance/customer.html.php
@@ -267,9 +267,9 @@
                                         <td><input class='$id' type='text' value='$eircode' disabled></td>
                                         <td>
                                             <img src='../../assets/ammendCustomer.svg' id='edit$id' class='visible' onclick=(editCustomer($id))>
-                                            <img src='../../assets/no.svg' id='cancel$id' class='hidden' onclick=()>
+                                            <img src='../../assets/no.svg' id='cancel$id' class='visible' onclick=()>
                                         </td>
-                                        <td><img src='../../assets/yes.svg' id='ammend$id' class='hidden' onclick=()></td>
+                                        <td><img src='../../assets/yes.svg' id='ammend$id' class='visible' onclick=()></td>
                                     </tr>
                                 ";
                             }
