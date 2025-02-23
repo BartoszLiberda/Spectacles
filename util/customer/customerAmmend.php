901214cf74118cc028aed003d064c0b5e79cd465
BartoszLiberda <C00295791@setu.ie>
2025-02-23 19:40:35 +0000

    another fix

--- util/customer/customerAmmend.php
+++ util/customer/customerAmmend.php
@@ -1,7 +1,7 @@
 <?php
     include('../db.inc.php');
 
-    $id = $_POST[idA];
+    $id = '$_POST[idA]';
     
     $sql = "UPDATE Customer SET FirstName = '$_POST[nameA]', Surname = '$_POST[sNameA]', Eircode = '$_POST[eircodeA]', Address = '$_POST[addressA]', DateOfBirth = '$_POST[dobA]', PhoneNumber = '$_POST[phoneA]'
     WHERE CustomerID = $id";
