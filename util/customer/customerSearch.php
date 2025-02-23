12d92205ef9183f973f2cd253f88bd505604e774
BartoszLiberda <C00295791@setu.ie>
2025-02-23 16:48:43 +0000

    search fix

--- util/customer/customerSearch.php
+++ util/customer/customerSearch.php
@@ -3,7 +3,7 @@
 
 <?php
     session_start();
-    
+
     $_SESSION["customerSearchSQL"] = "SELECT CustomerID, FirstName, Surname, DateOfBirth, PhoneNumber, Address, Eircode FROM Customer WHERE Del_Tag = 0";
 
     if($_POST['IDS'] != null){
@@ -11,27 +11,27 @@
     }
 
     if($_POST['fNameS'] != null){
-        $_SESSION["customerSearchSQL"] = $_SESSION["customerSearchSQL"] .= " and $_POST[fNameS] = FirstName";
+        $_SESSION["customerSearchSQL"] = $_SESSION["customerSearchSQL"] .= " and FirstName = '$_POST[fNameS]'";
     }
 
     if($_POST['sNameS'] != null){
-        $_SESSION["customerSearchSQL"] = $_SESSION["customerSearchSQL"] .= " and Surname = $_POST[sNameS]";
+        $_SESSION["customerSearchSQL"] = $_SESSION["customerSearchSQL"] .= " and Surname = '$_POST[sNameS]'";
     }
 
     if($_POST['dobS'] != null){
-        $_SESSION["customerSearchSQL"] = $_SESSION["customerSearchSQL"] .= " and DateOfBirth = $_POST[dobS]";
+        $_SESSION["customerSearchSQL"] = $_SESSION["customerSearchSQL"] .= " and DateOfBirth = '$_POST[dobS]'";
     }
 
     if($_POST['pNumS'] != null){
-        $_SESSION["customerSearchSQL"] = $_SESSION["customerSearchSQL"] .= " and PhoneNumber = $_POST[pNumS]";
+        $_SESSION["customerSearchSQL"] = $_SESSION["customerSearchSQL"] .= " and PhoneNumber = '$_POST[pNumS]'";
     }
 
     if($_POST['AddressS'] != null){
-        $_SESSION["customerSearchSQL"] = $_SESSION["customerSearchSQL"] .= " and Address = $_POST[AddressS]";
+        $_SESSION["customerSearchSQL"] = $_SESSION["customerSearchSQL"] .= " and Address = '$_POST[AddressS]'";
     }
 
     if($_POST['EircodeS'] != null){
-        $_SESSION["customerSearchSQL"] = $_SESSION["customerSearchSQL"] .= " and Eircode = $_POST[EircodeS]";
+        $_SESSION["customerSearchSQL"] = $_SESSION["customerSearchSQL"] .= " and Eircode = '$_POST[EircodeS]'";
     }
 
     header("Location: ../../pages/maintenance/customer.html.php");
