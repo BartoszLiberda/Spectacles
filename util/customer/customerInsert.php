f0c5861ab710c4f24e01c80097f60b164dcd0fdc
BartoszLiberda <C00295791@setu.ie>
2025-02-24 11:18:05 +0000

    date insert fix

--- util/customer/customerInsert.php
+++ util/customer/customerInsert.php
@@ -5,7 +5,7 @@
     include('../db.inc.php');
     date_default_timezone_set("UTC");
 
-   $dob = date_create($_POST('dob'));
+   $dob = date_create(date_format($_POST('dob'),"d-m-Y"));
     
     $sql = "Insert into Customer (FirstName,Surname,Eircode,Address,DateOfBirth,PhoneNumber,Del_Tag)
     VALUES ('$_POST[fname]','$_POST[sname]','$_POST[eircode]','$_POST[address]',$dobxs,'$_POST[phone]','0')";
