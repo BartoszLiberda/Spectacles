904f607bb8bcf9b00f94fedcfd3c401fd993d27e
BartoszLiberda <C00295791@setu.ie>
2025-03-25 12:11:19 +0000

    fix

--- util/customer/customerDelete.php
+++ util/customer/customerDelete.php
@@ -11,6 +11,14 @@
         die("An Error in the SQL Query : " . mysqli_error($con));
     }
     
+    unset($_SESSION['customerIDDelete']); // clears the session variable 
+    unset($_SESSION['customerFirstNameDelete']); // clears the session variable
+    unset($_SESSION['customerSurnameDelete']); // clears the session variable
+    unset($_SESSION['customerEircodeDelete']); // clears the session variable
+    unset($_SESSION['customerAddressDelete']); // clears the session variable
+    unset($_SESSION['customerDOBDelete']); // clears the session variable
+    unset($_SESSION['customerPhoneDelete']); // clears the session variable
+
     mysqli_close($con);
     header("Location: ../../pages/maintenance/customer.html.php");
     exit();
