21a8e5e7bd7b30167bcc7a34809fda94adbdebb3
BartoszLiberda <C00295791@setu.ie>
2025-03-25 12:01:29 +0000

    fix

--- util/customer/customer.js
+++ util/customer/customer.js
@@ -179,7 +179,6 @@ function editCustomerAmmend(className){
 }
 
 function customerDelete(){
+    document.getElementById("deleteCustomer").action = "customer.html.php?deleted=true";
     document.getElementById("deleteCustomer").submit();
-    document.getElementById("findButton").hidden = true;
-    document.getElementById("deleteButton").removeAttribute('hidden');
 }
--- util/customer/customerDelete.php
+++ util/customer/customerDelete.php
@@ -1,7 +1,7 @@
 <?php
     include('../db.inc.php');
 
-    $id = $_POST['customerID'];
+    $id = $_SESSION['customerIDDelete'];
 
     $sqlLookUp = "SELECT * FROM EyeTest WHERE CustomerID = $id and Del_Tag = 0";
     $row = mysqli_fetch_array($result);
@@ -11,31 +11,6 @@
         die("An Error in the SQL Query : " . mysqli_error($con));
     }
     
-    $rowcount = mysqli_affected_rows($con); // number of rows that were returned 1 if something was returned 0 if not
-
-    if($rowcount == 1) // if something was found in the db
-    {
-        $row = mysqli_fetch_array($result); // gets the rows from the table that was returned from the db
-
-        $_SESSION['customerIDDelete'] = $row['CustomerID']; // sets the session variable to variable fetched from db
-        $_SESSION['customerFirstNameDelete'] = $row['FirstName']; // sets the session variable to variable fetched from db
-        $_SESSION['customerSurnameDelete'] = $row['Surname']; // sets the session variable to variable fetched from db
-        $_SESSION['customerEircodeDelete'] = $row['Eircode']; // sets the session variable to variable fetched from db
-        $_SESSION['customerAddressDelete'] = $row['Address']; // sets the session variable to variable fetched from db
-        $_SESSION['customerDOBDelete'] = $row['DateOfBirth']; // sets the session variable to variable fetched from db
-        $_SESSION['customerPhoneDelete'] = $row['PhoneNumber']; // sets the session variable to variable fetched from db
-    }
-    else if ($rowcount == 0) // if nothing was found
-    {
-        unset($_SESSION['customerIDDelete']); // clears the session variable 
-        unset($_SESSION['customerFirstNameDelete']); // clears the session variable
-        unset($_SESSION['customerSurnameDelete']); // clears the session variable
-        unset($_SESSION['customerEircodeDelete']); // clears the session variable
-        unset($_SESSION['customerAddressDelete']); // clears the session variable
-        unset($_SESSION['customerDOBDelete']); // clears the session variable
-        unset($_SESSION['customerPhoneDelete']); // clears the session variable
-    }
-
     mysqli_close($con);
     header("Location: ../../pages/maintenance/customer.html.php");
     exit();
